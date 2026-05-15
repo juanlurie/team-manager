587418f2b024126c971d437c42f07604f439045a
Developer <dev@teammanager.local>
2026-05-15 19:37:46 +0000

    refactor: move leave fetch config to dedicated appsettings.LeaveFetch.json

--- src/TeamManager.Api/obj/Debug/net9.0/TeamManager.Api.AssemblyInfo.cs
+++ src/TeamManager.Api/obj/Debug/net9.0/TeamManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4495dfb8635ca307439046d0df9ad431517af037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b550eb9d1d107df423cb8f16cd0fd125b96a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
