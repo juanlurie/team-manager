ba50ceff34e1954cf9fefd9b5370cc5a5e8e8069
Developer <dev@teammanager.local>
2026-06-23 18:37:43 +0000

    Revert accidental node_modules churn from the Wordle commit
    The previous commit's git add picked up unrelated node_modules drift
    (different npm install states accumulated across this session) -- this
    restores node_modules to match the commit before it, touching nothing
    else.

--- team-manager-ui/node_modules/lmdb/dependencies/lz4/lib/lz4frame_static.h
+++ team-manager-ui/node_modules/lmdb/dependencies/lz4/lib/lz4frame_static.h
@@ -1,7 +1,7 @@
 /*
    LZ4 auto-framing library
    Header File for static linking only
-   Copyright (c) Yann Collet. All rights reserved.
+   Copyright (C) 2011-2020, Yann Collet.
 
    BSD 2-Clause License (http://www.opensource.org/licenses/bsd-license.php)
 
